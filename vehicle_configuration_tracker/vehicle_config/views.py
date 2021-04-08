e23fa3ddde1d91df746defdb686ad103a2bfc90c
Darsh Kale <kale0085@umn.edu>
2021-04-08 13:31:28 -0500

    added files to static

--- vehicle_configuration_tracker/vehicle_config/views.py
+++ vehicle_configuration_tracker/vehicle_config/views.py
@@ -1,12 +1,18 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-import datetime
+from datetime import datetime
+from .models import Vehicle, Setup
+import json
+from django.core import serializers
 
 # Create your views here.
 
-
-
 def home(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    vehicles = Vehicle.objects.all()
+    setups = Setup.objects.all()
+    setups = serializers.serialize("json",setups)
+    context = {
+        'vehicles' : vehicles,
+        'setups' : json.dumps(setups)
+    }
+    return render(request, 'vehicle_config/home.html', context)
+
